8626ffb4bb523f02641646fb035f744160360235
r-a-y <r-a-y@gmx.com>
2016-09-20 12:08:13 -0700

    Hide MEXP-Vimeo drag-n-drop upload instructions when using Press This.
    See #1.

--- external-media-button.php
+++ external-media-button.php
@@ -35,6 +35,8 @@ class Ray_External_Media_Button {
 	protected function __construct() {
 		add_action( 'media_buttons',    array( $this, 'add_media_button' ) );
 		add_action( 'wp_enqueue_media', array( $this, 'enqueue_assets' ) );
+
+		add_action( 'admin_head',       array( $this, 'press_this_inline_css' ) );
 	}
 
 	/**
@@ -66,4 +68,28 @@ class Ray_External_Media_Button {
 
 		wp_enqueue_style( 'ray-media', $base . 'modal.css' );
 	}
+
+	/**
+	 * Add inline CSS when on the "Press This" page.
+	 */
+	public function press_this_inline_css() {
+		// Do not enqueue CSS when on "Press This" page.
+		if ( false === strpos( $_SERVER['REQUEST_URI'], '/press-this.php' ) ) {
+			return;
+		}
+
+		// This doesn't really work at the moment, but leave it here anyway.
+		add_filter( 'mexp_vimeo_uploader_css_class', '__return_empty_string', 999 );
+
+		$inline_css = <<<EOD
+
+.press-this .mexp-content-vimeo-vupload .drop-instructions {
+	display: none;
+}
+
+EOD;
+
+		printf( '<style type="text/css">%s</style>', $inline_css );
+	}
+
 }
